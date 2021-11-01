e7f68ef6a9b0a0b680355b570e7322b2eaaded5a
Jennifer Nelson <jennifer.nelson@integrate.com>
2021-11-01 16:41:42 -0500

    Home page: Add links to posts

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link'
+import Date from '../components/date'
 
 //gets data at build time
 export async function getStaticProps() {
@@ -42,11 +44,13 @@ export default function Home({allPostsData}) {
           <ul className={utilStyles.list}>
             {allPostsData.map(({ id, date, title }) => (
               <li className={utilStyles.listItem} key={id}>
-                {title}
+                <Link href={`/posts/${id}`}>
+                  <a>{title}</a>
+                </Link>
                 <br />
-                {id}
-                <br />
-                {date}
+                <small className={utilStyles.lightText}>
+                  <Date dateString={date} />
+                </small>
               </li>
             ))}
           </ul>
